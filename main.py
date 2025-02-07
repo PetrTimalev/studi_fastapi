0681ab2b62fe8de06d01cb5ca5b89e64fe8dcd9d
petrtimalev <petr.timalev@gmail.com>
2025-02-07 11:18:40 +1000

    Валидация с помощью RegEx

--- main.py
+++ main.py
@@ -3,20 +3,15 @@ from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
-
-# @app.get("/user/{username}")
-# async def login(
-#         username: Annotated[str, Path(min_length=3, max_length=15, description='Enter your username', example='permin0ff')],
-#         first_name: Annotated[str | None, Query(max_length=10)] = ...) -> dict:
-#     return {"user": username, "Name": first_name}
-
-
-# http://127.0.0.1:8000/user?people=tom&people=Sam&people=Bob
-# {"user":"Timalev","Name":"Petr"}
-@app.get("/user")
-async def search(people: Annotated[list[str], Query()]) -> dict:
-    return {"user": people}
-
+#  включим параметр pattern в функцию Query(), чтобы ограничить значение first_name либо начинаться
+#  с J, либо заканчиваться на s (как например John, Tomas).
+@app.get("/user/{username}")
+async def login(
+        username: Annotated[
+            str, Path(min_length=3, max_length=15, description='Enter your username', example='permin0ff')],
+        first_name: Annotated[
+            str | None, Query(max_length=10, pattern="^J|s$")] = None) -> dict:
+    return {"user": username, "Name": first_name}
 
 if __name__ == "__main__":
     import uvicorn
