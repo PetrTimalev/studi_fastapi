4c0f2a593724101a165e4e9c96570483db527b59
petrtimalev <petr.timalev@gmail.com>
2025-02-18 10:58:36 +1000

    post request

---
+++ project1.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, status, Body
+from fastapi import FastAPI, status
+
+
+app = FastAPI()
+
+
+messages_db = {0: "First post in FastAPI"}
+
+
+@app.get("/")
+async def get_all_messages() -> dict:
+    return messages_db
+
+
+@app.get("/message/{message_id}")
+async def get_message(message_id: int) -> str:
+    return messages_db[message_id]
+
+
+# @app.post("/message")
+# async def create_message(message: str) -> str:
+#     return message
+
+# @app.post("/message", status_code=status.HTTP_201_CREATED)
+# async def create_message(message: str) -> str:
+#     current_index = int(max(messages_db, key=int)) + 1
+#     messages_db[current_index] = message
+#     return "Message created!"
+
+@app.post("/message", status_code=status.HTTP_201_CREATED)
+async def create_message(message: str = Body()) -> str:
+    current_index = int(max(messages_db, key=int)) + 1
+    messages_db[current_index] = message
+    return "Message created!"
+
+@app.put("/message/{message_id}")
+async def update_message(message_id: int, message: str) -> str:
+    pass
+
+
+@app.delete("/message/{message_id}")
+async def delete_message(message_id: int) -> str:
+    pass
+
+
+@app.delete("/")
+async def kill_message_all() -> str:
+    pass
