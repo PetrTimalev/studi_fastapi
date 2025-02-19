19b5cd354611d8a9567c2b86eb0cd20bbfc2a73a
petrtimalev <petr.timalev@gmail.com>
2025-02-20 09:01:33 +1000

    update model

--- models.py
+++ models.py
@@ -1,30 +1,10 @@
-from datetime import date
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel
 
 
-class User(BaseModel):
-    id: int
-    name: str
-    birthday_date: date
+class Message(BaseModel):
+    id: int | None = None
+    text: str
 
-    @field_validator('name', mode='before')
-    def validate_name(cls, v):
-        if isinstance(v, int):
-            return str(v)
-        elif isinstance(v, str):
-            return v
-        else:
-            raise ValueError("Имя должно быть строкой или числом")
-
-
-dima = User(
-    id="3",
-    name=123,
-    birthday_date="1990-11-22"
-)
-
-to_dict = dima.model_dump()
-to_json = dima.model_dump_json()
-
-print(to_dict, type(to_dict))
-print(to_json, type(to_json))
+    model_config = {
+        "json_schema_extra": {"examples": [{"text": "Simple message", }]}
+    }
